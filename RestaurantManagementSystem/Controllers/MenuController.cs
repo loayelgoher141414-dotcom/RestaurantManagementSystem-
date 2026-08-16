3a86f257ff1179e237dacc5eab14f88a43812946
sama <sama.amer.cv@gmail.com>
2026-08-16 06:00:15 +0300

    Implement menu module

---
+++ RestaurantManagementSystem/Controllers/MenuController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantManagementSystem.Models;
+
+namespace RestaurantManagementSystem.Controllers
+{
+    public class MenuController : Controller
+    {
+        RMSContext _context = new RMSContext();
+
+        public IActionResult Index()
+        {
+            var menuItems = _context.MenuItems.ToList();
+
+            return View(menuItems);
+        }
+    }
+}
